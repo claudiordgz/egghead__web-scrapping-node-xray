311aed55fc491f2b8944ccabad086e059e93e81e
claudiordgz <claudio.rdgz@gmail.com>
2017-04-30 19:01:26 -0400

    feat: added redis connection

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const config = require('./config/config')
 const Xray = require('x-ray')
-const fs = require('fs')
 const winston = require('winston')
 const magnetLink = require('magnet-link')
-const xray = new Xray()
+const redis = require('redis')
 
 function getMagnetLink (item) {
   return new Promise((resolve, reject) => {
@@ -24,34 +24,69 @@ function mergeMagnetArrayToResults (magnetArray, results) {
   return lResults
 }
 
-xray('http://bt.ocremix.org/index.php?order=date&sort=descending', '.trkInner tr:not(:first-child)',
-  [{
-    name: '.colName a',
-    torrentUri: '.colName a@href',
-    size: '.colSize',
-    info: '.colName .torrentTag:last-child a@href',
-    added: '.colAdded',
-    category: '.colCategory'
-  }])((err, results) => {
-    if (err) {
-      winston.error('no results from ocremix', err)
-    }
+function getOCReMixData () {
+  return new Promise((resolve, reject) => {
+    const xray = new Xray()
+    xray('http://bt.ocremix.org/index.php?order=date&sort=descending', '.trkInner tr:not(:first-child)',
+      [{
+        name: '.colName a',
+        torrentUri: '.colName a@href',
+        size: '.colSize',
+        info: '.colName .torrentTag:last-child a@href',
+        added: '.colAdded',
+        category: '.colCategory'
+      }])((err, results) => {
+        if (err) {
+          winston.error('no results from ocremix', err)
+        }
 
-    results = results
+        results = results
           .filter((item) => item.torrentUri.length !== 0)
-    let promises = results
+        let promises = results
             .map(getMagnetLink)
 
-    Promise.all(promises)
+        Promise.all(promises)
           .then((magnets) => {
             const resJSON = mergeMagnetArrayToResults(magnets, results)
             const resString = JSON.stringify(resJSON, null, '\t')
-            fs.writeFile('./results.json', resString, 'utf8', (err) => {
-              if (err) {
-                winston.log('error', 'couldn\'t write results')
-              } else {
-                winston.log('info', 'wrote results json')
-              }
-            })
+            resolve(resString)
           })
+              .catch((err) => {
+                reject(err)
+              })
+      })
   })
+}
+
+function getCache (redisClient, key) {
+  return new Promise((resolve, reject) => {
+    redisClient.get(key, (err, reply) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(reply)
+      }
+    })
+  })
+}
+
+async function readOCReMix (environmentName) {
+  let configuration = config.loadConfiguration(environmentName || 'development')
+  let payload = await getOCReMixData()
+  let redisClient = redis.createClient(configuration.redis.PORT, configuration.redis.HOSTNAME)
+  redisClient.auth(configuration.redis.PASSWORD, (err) => {
+    if (err) {
+      throw err
+    }
+  })
+  let cachedData = await getCache(redisClient, configuration.redis.KEY)
+  if (cachedData === null || cachedData !== payload) {
+    winston.log('info', 'New OCReMix Payload')
+    redisClient.set(configuration.redis.KEY, payload)
+  } else {
+    winston.log('warning', 'No New OCReMix Payload')
+  }
+  redisClient.quit()
+}
+
+readOCReMix()
